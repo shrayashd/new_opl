70c51306eafa80bf1095a8f8a07a9d40a092e430
Shrayashd <shrayash000@gmail.com>
2025-07-09 16:45:36 +0545

    image upload fix

--- resources/views/frontend/home/index.blade.php
+++ resources/views/frontend/home/index.blade.php
@@ -3,7 +3,7 @@
     <section class="hero-style1">
         <div class="hero-bg"
             style="background-image: url('{{ asset('frontend/assets/img/bg/bg3
-                                                                                                                                                                                                                                                                                                                                                .jpg') }}');">
+                                                                                                                                                                                                                                                                                                                                                            .jpg') }}');">
         </div>
 
         <div class="hero-leaf2 wow fadeInUp" data-wow-delay="1s">
@@ -407,7 +407,7 @@
                                     </a>
                                 </h3>
                                 <p class="feature-text">
-                                    Sed perspiciatis unde is tus error voluptatem
+                                    Sed perspiciatis is tus error voluptatem
                                 </p>
                             </div>
                         </div>
